8804d5ff60417bb5151f7839bfb733c0e309ec00
Juan Ayma <juan.ayma@unmsm.edu.pe>
2023-06-29 22:17:59 -0500

    Creacion de archivos para los sensores y actuadores como tambien para el ESP8266

---
+++ esp8266/esp8266.ino
@@ -0,0 +1,189 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+SoftwareSerial SerialESP8266(13, 15); // RX, TX
+
+const int lonbuffer = 12;                                       // longitud del buffer
+char buffer[lonbuffer];                                         // buffer para almacenar el comando
+float luminosidad;                                              // valor del tercer parámetro
+
+#define LED 16
+
+
+//Configuracion del user
+const char* ssid = "TP_Ayma";
+const char* password = "09089327";
+
+char data_temp[12] = "";
+char data_humi[12] = "";
+char data_humiSuelo[12] = "";
+char data_digital[12] = "";
+char data_dist[12] = "";
+char data_lumi[12] = "";
+
+//IP del servidor BROKER
+const char *mqtt_broker = "192.168.0.110";
+
+//const char *mqtt_broker = "15.229.78.220";
+const int mqtt_port = 1883;
+//const char* mqtt_server="test.mosquitto.org";
+
+
+/*
+  // MQTT Broker
+  const char *mqtt_broker = "broker.emqx.io";
+  const char *mqtt_username = "emqx";
+  const char *mqtt_password = "publico";
+  const int mqtt_port = 1883;
+*/
+
+//Inicializamos el nodeMCU para wifi y Mqtt
+WiFiClient esp82;
+PubSubClient client(esp82);
+
+long lastMsg = 0;
+long tdhtx = 0;
+char msg[50];
+int value = 0;
+
+//********************Funciones para Mqtt******************
+
+//--------------------Configuración Wifi-------------------
+void config_wifi() {
+  //Conectandose a una red Wifi
+  Serial.println("Conectandose a la red ...");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // Mientras el ESP82 no se conecte al AP:
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("ESP82 conectado, su IP es: ");
+  Serial.println(WiFi.localIP());
+}
+//--------------------Lectura de datos que llegan despues de suscribir-------------------
+void callback(String topic, byte* message, unsigned int length) {
+  //Serial.println("Mensaje que llega del topic: ");
+  Serial.print("Mensaje que llega del topic[");
+  Serial.print(topic);
+  Serial.print("] ");
+  //Serial.println(". Message: ");
+  String messageData = "";
+
+  for (int i = 0; i < length; i++) {
+    //Serial.println((char)message[i]);
+    Serial.print((char)message[i]);
+    messageData += (char)message[i];
+  }
+  Serial.println();
+  Serial.print("Tamaño del mensaje :");
+  Serial.println(length);
+  Serial.println();
+  Serial.println("-----------------------");
+  Serial.println(messageData);
+
+  //ACTUADORES EL LED
+  if (messageData == "ON") {
+    Serial.println("LED");
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
+
+}
+
+//--------------------Conexión a broker/ Suscribir-------------------
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Iniciando conexión con Broker...");
+    String clienteId = "ESP82";
+    if (client.connect(clienteId.c_str())) {
+      //if (client.connect(clienteId.c_str(), mqtt_username, mqtt_password)) {
+      client.subscribe("esp82iot");//topic para suscribir
+      //client.subscribe("esp82iot/");//topic para suscribir
+      Serial.println("Conexión exitosa");
+    }
+    else {
+      Serial.println("Failed, rc=");
+      Serial.print(client.state());
+      Serial.print(" esperando 3 segundos");
+      delay(3000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  SerialESP8266.begin(9600);
+  config_wifi();
+  //client.setServer(mqtt_broker,1883);//Conexión al servidor/Broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);//Llamada a los callback para ver si recibo mensajes del broker
+}
+
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  checkSerialCom();
+}
+
+void checkSerialCom() {
+  if (SerialESP8266.available() > 0)
+  {
+    SerialESP8266.readBytesUntil('T', buffer, lonbuffer);
+    float t = SerialESP8266.parseFloat();
+
+    SerialESP8266.readBytesUntil('H', buffer, lonbuffer);
+    float h = SerialESP8266.parseFloat();
+
+    SerialESP8266.readBytesUntil('L', buffer, lonbuffer);
+    float l = SerialESP8266.parseFloat();
+
+    SerialESP8266.readBytesUntil('VHS', buffer, lonbuffer);
+    float valHumsuelo = SerialESP8266.parseFloat();
+
+    SerialESP8266.readBytesUntil('U', buffer, lonbuffer);
+    int distancia = SerialESP8266.parseInt();
+
+    sprintf(data_temp, "%3.2f", t); //dar formato a un numero entero, flotante, double, etc a String (3 enteros.2 decimales flotantes)
+    client.publish("temperatura", data_temp); //el topic se llama temperatura
+    Serial.print("Publish message temperatura: ");
+    Serial.println(data_temp);//msg
+
+    sprintf(data_humi, "%3.2f", h); //dar formato a un numero entero, flotante, double, etc a String (3 enteros.2 decimales flotantes)
+    client.publish("humedad", data_humi); //el topic se llama humedad
+    Serial.print("Publish message humedad: ");
+    //Serial.print("Humedad: ");
+    Serial.println(data_humi);//msg
+
+    sprintf(data_lumi, "%3.2f", l); //dar formato a un numero entero, flotante, double, etc a String (3 enteros.2 decimales flotantes)
+    client.publish("luminosidad", data_lumi); //el topic se llama luminosidad
+    Serial.print("Publish message luminosidad: ");
+    //Serial.print("Humedad: ");
+    Serial.println(data_lumi);//msg
+
+    sprintf(data_humiSuelo, "%3.2f", valHumsuelo); //dar formato a un numero entero, flotante, double, etc a String (3 enteros.2 decimales flotantes)
+    client.publish("humedadSuelo", data_humiSuelo); //el topic se llama humedadSuelo
+    Serial.print("Publish message humedadSuelo: ");
+    //Serial.print("humedadSuelo: ");
+    Serial.println(data_humiSuelo);//msg
+
+    sprintf(data_dist, "%u", distancia); //dar formato a un numero entero, flotante, double, etc a String (3 enteros.2 decimales flotantes)
+    client.publish("distanciaObj", data_dist); //el topic se llama distanciaObj
+    Serial.print("Publish message distanciaObj: ");
+    Serial.print(data_dist);//msg
+    Serial.println(" cm");
+  }
+}
---
+++ megaConex/megaConex.ino
@@ -0,0 +1,160 @@
+/*ZONA DE INCLUSIÓN DE LIBRERIAS*/
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <SoftwareSerial.h>
+#include <NewPing.h>
+
+/*DEFINICIÓN DE PINES*/
+#define rxPin 2                                                       //Pines para el envío se Arduino Uno a NodeMCU.
+#define txPin 3
+
+//Sensor de humedad DHT22
+#define DHT_PIN 5 // Se define el pin para conectar el sensor DHT22
+#define DHT_TIPO DHT22 //Definimos el modelo del sensor DHT22
+#define LED 16
+
+//Sensor LDR
+#define LDR_PIN A0
+
+//Sensor FC-28
+#define sensorFCPin A1 // Sensor FC-28 pin analogico
+#define pinfc28V  9 //control de alimentacion
+int humedadSuelo = 0;
+float valHumsuelo;
+
+//PIR
+int pinPir = 8;
+int val = 0; //valor que recibimos del pir
+int pirEstado = LOW; //estado inicial del pir, no hay movimiento
+
+//pines SensorUltrasonico
+#define TRIG_PIN  6      // trigger
+#define  ECO_PIN  7      // echo
+unsigned long tiempo, distancia;
+NewPing sonar(TRIG_PIN, ECO_PIN);
+
+
+/*DECLARACIÓN DE VARIABLES GLOBALES*/
+unsigned int valor_medido;                                      //Valor obtenido al leer por A0.
+int ledPin = 10;                                                     //Pin de conexión del led de prueba.
+float LR;                                                               //Variable para almacenar la luminosidad relativa.
+
+
+/*PROTOTIPADO*/
+SoftwareSerial Trans(rxPin, txPin);               //Definimos puerto serie virtual. Pines para Rx y Tx.
+DHT dht(DHT_PIN, DHT_TIPO);                        //Nombramos y configuramos el sensor para poder inicializarlo.
+
+/*Variables DHT22*/
+float t;
+float h;
+
+long tdhtx = 0;
+
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode (txPin , OUTPUT);
+  Serial.begin(9600);
+  Trans.begin(9600);                            //Velocidad de envío para la transmisión al NodeMCU.
+  Serial.println("DHT22 test!");
+  //Configuramos el los pines FC-28
+  pinMode(sensorFCPin, INPUT);
+  pinMode(pinfc28V, OUTPUT);
+  dht.begin();                                  //Arrancamos el sensor.
+}
+
+
+void loop()
+{
+  long tiempo = millis();
+  //messageData="";
+
+  sensorPir();
+  if (tiempo - tdhtx > 2000) { //Envia cada 2 segundos
+    tdhtx = tiempo;
+
+    sensorDHT();
+    sensorLDR();
+    sensorFC28();
+    sensorUltrasonico();
+    Trans.println('T' + String(t) + 'H' + String(h) + 'L' + String(LR) + "VHS" + String(valHumsuelo) + "U" + String(distancia));
+    Serial.print("Temperatura: ");
+    Serial.println(t);
+    Serial.print("Humedad: ");
+    Serial.println(h);
+    Serial.print("Luminosidad: ");
+    Serial.println(LR);
+    Serial.print("Humedad Suelo: ");
+    Serial.println(valHumsuelo);
+    Serial.print("Distancia: ");
+    Serial.println(distancia);
+  }
+}
+
+void sensorDHT() {
+  //Enviando humedad y temperatura
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+}
+void sensorLDR() {
+  valor_medido = analogRead(LDR_PIN);                      //Valor de luminosidad medido en A0
+  LR = (valor_medido * 100.0) / 1023.0;               //Luminosidad medida en tanto por ciento
+  if (isnan(LR)) {
+    Serial.println(F("Failed to read from LDR sensor!"));
+    return;
+  }
+}
+
+void sensorFC28() {
+
+  //Enviando humedad y temperatura
+  //Aplique energía al sensor de humedad del suelo
+  digitalWrite(pinfc28V, HIGH);
+  delay(10); // espera de 10 milisegundos
+  humedadSuelo = analogRead(sensorFCPin);
+  // Apague el sensor para reducir la corrosión del metal
+  //con el tiempo
+  digitalWrite(pinfc28V, LOW);
+  //Convertir el valor en porcentaje
+  valHumsuelo = map(humedadSuelo, 1023, 0, 0, 100);
+  if (isnan(valHumsuelo)) {
+    Serial.println(F("Failed to read from FC-28 sensor!"));
+    return;
+  }
+}
+
+void sensorPir()
+{
+  //Sensor de Movimiento
+  val = digitalRead(pinPir);
+  if (val == HIGH) { //si está activado
+    //digitalWrite(pinLED,HIGH);//encender LED movimiento
+    if (pirEstado == LOW) { //si previamente estaba apagado
+      Serial.println("Sensor activado");
+      pirEstado = HIGH;
+    }
+  } else { //si está desactivado
+    //digitalWrite(pinLED,LOW);
+    if (pirEstado == HIGH) { //si previamente estaba encendido
+      Serial.println("Sensor apagado");
+      pirEstado = LOW;
+    }
+  }
+}
+
+void sensorUltrasonico() {
+
+  tiempo = sonar.ping_median(5);//tiempo promedio de ir y venir del trigger
+  distancia = tiempo / US_ROUNDTRIP_CM;
+
+  if (isnan(distancia)) {
+    Serial.println(F("Failed to read from sensorUltrasonico!"));
+    return;
+  }
+  delay(50);
+}
